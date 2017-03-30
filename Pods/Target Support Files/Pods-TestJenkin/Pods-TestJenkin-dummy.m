6afa19c280b65398ae3733ffa8ad9a9036cffcd1
Ganesan <garunach@amgen.com>
2017-03-30 18:25:40 +0530

    Schema

---
+++ Pods/Target Support Files/Pods-TestJenkin/Pods-TestJenkin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestJenkin : NSObject
+@end
+@implementation PodsDummy_Pods_TestJenkin
+@end
---
+++ Pods/Target Support Files/Pods-TestJenkinTests/Pods-TestJenkinTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TestJenkinTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TestJenkinTestsVersionString[];
+
